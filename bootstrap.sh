1ab1f8dada23b66fe95c67b9e523dc78e0dcf6ad
Ben Coleman <benjamin.coleman@me.com>
2018-07-26 12:44:45 +0100

    changed to a shallow clone of cairis, removed apt installs as should be in cairis box

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,11 @@ locale-gen en_GB.UTF-8
 # Update repo list
 apt update
 
-# Set root password for mysql database (default is root)
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-
-# Install cairis dependencies
-apt-get install -y python-dev build-essential mysql-server mysql-client graphviz docbook dblatex python-pip python-numpy git libmysqlclient-dev --no-install-recommends texlive-latex-extra docbook-utils inkscape libxml2-dev libxslt1-dev poppler-utils python-setuptools
-
 # Copy the service file to the systemd directory
 mv ./cairis.service /etc/systemd/system/cairis.service
 
 # Clone the latest on the master branch
-git clone https://github.com/failys/cairis
+git clone --depth 1 https://github.com/failys/cairis
 cd cairis
 
 # Unsure if needed, but wheel was failing the installs
@@ -25,17 +18,14 @@ pip install -U wheel
 pip install -r requirements.txt
 
 # Environment variables for cairis
-# export CAIRIS_CFG=/home/vagrant/cairis.cnf
-# export PYTHONPATH=${PYTHONPATH}:/home/vagrant/cairis/
-
+export CAIRIS_CFG=/home/vagrant/cairis.cnf
+export PYTHONPATH=${PYTHONPATH}:/home/vagrant/cairis/
 
 ##########
 ## TODO ##
 ##########
-# Find out why pip install requirements is exiting...
 
 # Need to find a way to run ./quick_setup.py programatically
 # Can take from the docker examples if deciphered.
 
-systemctl enable /etc/systemd/system/cairis.service
-# systemctl start cairis.service
+# systemctl enable --now /etc/systemd/system/cairis.service
